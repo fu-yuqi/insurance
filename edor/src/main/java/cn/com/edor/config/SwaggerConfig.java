aa48dfea1b7f8f36325e2390bfa7b46c399a0f01
fu_yuqi@163.com <fu_yuqi@163.com>
2024-03-29 16:31:53 +0800

    解决部分警告

--- edor/src/main/java/cn/com/edor/config/SwaggerConfig.java
+++ edor/src/main/java/cn/com/edor/config/SwaggerConfig.java
@@ -5,20 +5,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.oas.annotations.EnableOpenApi;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
-import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @EnableSwagger2
 @EnableWebMvc  //解决springboot版本太高问题
--- edor/src/main/java/cn/com/edor/mapper/FuUserMapper.java
+++ edor/src/main/java/cn/com/edor/mapper/FuUserMapper.java
@@ -1,7 +1,6 @@
 package cn.com.edor.mapper;
 
 import cn.com.common.entity.first.FuUser;
-import cn.com.common.model.request.user.UserDo;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.springframework.stereotype.Repository;
 
