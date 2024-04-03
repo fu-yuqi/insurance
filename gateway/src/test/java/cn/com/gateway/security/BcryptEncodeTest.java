609f419c13e10384d2afb736c41fbd491f2f77f0
fu_yuqi@163.com <fu_yuqi@163.com>
2024-04-03 16:39:18 +0800

    17、引入spring security安全框架

---
+++ gateway/src/test/java/cn/com/gateway/security/BcryptEncodeTest.java
@@ -0,0 +1,32 @@
+package cn.com.gateway.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootTest
+public class BcryptEncodeTest {
+
+    // 测试加密
+    @Test
+    void bcryptEncode() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawPassword = "1234";
+        for (int i = 0; i < 50; i++) {
+            String encodePassword = passwordEncoder.encode(rawPassword);
+            System.err.println("encodePassword=" + encodePassword);
+        }
+    }
+    // 测试验证密码
+    @Test
+    void bcryptMatches() {
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String rawPassword = "1234";
+        String encodePassword =
+                "$2a$10$.eQiB/9PjQOQLfKJOG4vCekDoObxETfBir8hoRQpOA5/ggaAPSAxm";
+        boolean result = passwordEncoder.matches(rawPassword, encodePassword);
+        System.err.println("原文=" + rawPassword);
+        System.err.println("密码=" + encodePassword);
+        System.err.println("验证结果=" + result);
+    }
+}
